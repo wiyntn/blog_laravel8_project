3d93bfb395f1e6ddc47848dbeed4cbee1180d42e
Aananda <rmananda007@gmail.com>
2021-06-16 02:48:03 +0600

    foreach problem

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller{
+
+
+    public function index(){
+
+        $name="Ananda";
+        return view('welcome',compact('name'));
+    }
+
+    $student{
+        $name=>"Ananda",
+        $age=>27,
+        $gender=>"male",
+    }
+
+}
